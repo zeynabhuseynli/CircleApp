68bdc8aae1e5f34a6074f2fc43d3ff5807bc4758
zeynabhuseynli <zybhuseynli@gmail.com>
2025-04-24 12:33:28 +0400

    Creat status placeholder

--- CircleApp/obj/Debug/net8.0/CircleApp.AssemblyInfo.cs
+++ CircleApp/obj/Debug/net8.0/CircleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c822b7721a01f6e17434a64e4c01c04abdef8299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bd98d7e13ddeda813d2a440eca592f1bc07c5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CircleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
